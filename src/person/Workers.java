434a1d46250c411d076717160bec033de2b6ff7e
Weronika Graczyk <wercia01@onet.pl>
2022-01-25 16:18:53 +0100

    Add Workers

---
+++ src/person/Workers.java
@@ -0,0 +1,31 @@
+package person;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Workers implements Comparable<Workers>, Serializable {
+    private String login;
+    private String haslo;
+
+    public Workers(String login, String haslo) {
+        this.login = login;
+        this.haslo = haslo;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Workers workers = (Workers) o;
+        return Objects.equals(login, workers.login) && Objects.equals(haslo, workers.haslo);
+    }
+
+    @Override
+    public int compareTo(Workers o) {
+        return this.getLogin().compareTo(o.getLogin());
+    }
+}
